6c16638c658a1f65342bedc9c4da9d84a1c8b593
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-04-14 11:26:48 -0300

    Understanding routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello World!!!!!!' }));
+app.get('/courses', (request, response) =>
+  response.json(['Course 1', 'Course 2', 'Course 3'])
+);
+
+app.post('/courses', (request, response) =>
+  response.json(['Course 1', 'Course 2', 'Course 3', 'Course 4'])
+);
+
+app.put('/courses/:id', (request, response) =>
+  response.json(['Course 6', 'Course 2', 'Course 3', 'Course 4'])
+);
+
+app.patch('/courses/:id', (request, response) =>
+  response.json(['Course 6', 'Course 7', 'Course 3', 'Course 4'])
+);
+
+app.delete('/courses/:id', (request, response) =>
+  response.json(['Course 7', 'Course 3', 'Course 4'])
+);
 
 app.listen(3333);
