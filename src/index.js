b4b787418aff5dd9f682b457a643440a10fe0b45
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-04-14 11:15:33 -0300

    Using nodemon

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
+app.get('/', (req, res) => res.json({ message: 'Hello World!!!!!!' }));
 
 app.listen(3333);
