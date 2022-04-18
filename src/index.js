2f3d9c78fe61616e968925bd91d512bf77ac770e
Jonathan Alba Videira <jonathan.videira@gmail.com>
2022-04-17 22:19:57 -0300

    Understanding the parameters

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ const res = require('express/lib/response');
 
 const app = express();
 
-app.get('/courses', (request, response) =>
-  response.json(['Course 1', 'Course 2', 'Course 3'])
-);
+app.use(express.json());
 
-app.post('/courses', (request, response) =>
-  response.json(['Course 1', 'Course 2', 'Course 3', 'Course 4'])
-);
+app.get('/courses', (request, response) => {
+  console.log(request.query);
+  return response.json(['Course 1', 'Course 2', 'Course 3']);
+});
 
-app.put('/courses/:id', (request, response) =>
-  response.json(['Course 6', 'Course 2', 'Course 3', 'Course 4'])
-);
+app.post('/courses', (request, response) => {
+  console.log(request.body);
+  return response.json(['Course 1', 'Course 2', 'Course 3', 'Course 4']);
+});
+
+app.put('/courses/:id', (request, response) => {
+  console.log(request.params);
+  return response.json(['Course 6', 'Course 2', 'Course 3', 'Course 4']);
+});
 
 app.patch('/courses/:id', (request, response) =>
   response.json(['Course 6', 'Course 7', 'Course 3', 'Course 4'])
